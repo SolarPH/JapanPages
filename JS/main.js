0f073eea5917a3dc7a2985949bac977147155f44
SolarPH <49668966+SolarPH@users.noreply.github.com>
2021-10-14 09:25:13 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,6 +1,15 @@
 import TableCSV from "./TableCSV.js";
 
 const tableRoot = document.querySelector("#csvRoot");
+const csvFileInput = document.querySelector("#csvFileInput");
 const tableCsv = new TableCSV(tableRoot);
 
-tableCsv.setHeader(["ID", "Name", "Age"])
+csvFileInput.addEventListener("change", e => {
+  Papa.parse(csvFileInput.files[0], {
+    delimiter: ",",
+    skipEmptyLines: true,
+    complete: results => {
+      console.log(results);
+    }
+  })
+});
