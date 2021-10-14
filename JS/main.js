4e62745f437d30c932b142d2dc5b5ca5694307d6
SolarPH <49668966+SolarPH@users.noreply.github.com>
2021-10-14 09:34:14 +0800

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -5,11 +5,13 @@ const csvFileInput = document.querySelector("#csvFileInput");
 const tableCsv = new TableCSV(tableRoot);
 
 csvFileInput.addEventListener("change", e => {
+  console.log(csvFileInput.files[0]);
+  
   Papa.parse(csvFileInput.files[0], {
     delimiter: ",",
     skipEmptyLines: true,
     complete: results => {
       console.log(results);
     }
-  })
+  });
 });
